0952320d11f75bab8854632b4ebd13cb8a998a73
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-06-15 11:03:47 +0200

    feat(shuutils): add list script

---
+++ libs/shuutils/src/list.cli.ts
@@ -0,0 +1,23 @@
+import { writeFileSync } from 'node:fs'
+import glob from 'tiny-glob'
+import { Logger } from './lib/logger.js'
+import path from 'node:path'
+
+/**
+ * List entries in lib folder into the barrel index.ts file
+ * @returns {Promise<void>}
+ */
+async function listEntries() {
+  const logger = new Logger()
+  const lib = path.join(__dirname, 'lib')
+  const index = path.join(__dirname, 'index.ts')
+  logger.info('Listing entries in lib folder..., target: ', lib)
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  const files = await glob('*.ts', { cwd: lib, filesOnly: true })
+  const list = files.filter(file => !(file.includes('shuutils.ts') || file.includes('unique-mark.ts') || file.includes('.test.ts'))).map(file => `export ${file.includes('types') ? 'type ' : ''}* from './lib/${file.replace('.ts', '.js')}'`)
+  const content = `${list.sort().join('\n')}\n`
+  writeFileSync(index, content)
+  logger.success(`${index} updated !`)
+}
+
+await listEntries()
