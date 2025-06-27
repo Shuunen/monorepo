73a272861dd080bf20d1a885812d9a1d1265438e
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-06-27 23:05:57 +0200

    feat(stuff-finder): integrate uniqueMark in Vite config

--- apps/stuff-finder/vite.config.ts
+++ apps/stuff-finder/vite.config.ts
@@ -1,9 +1,10 @@
 import { preact } from '@preact/preset-vite'
+import { uniqueMark } from '@shuunen/vite-plugins'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [preact(), tailwindcss()],
+  plugins: [preact(), tailwindcss(), uniqueMark()],
   server: {
     port: 8080,
   },
