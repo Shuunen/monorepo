44f87cc0095dbc95a0f3c4a28ba725ffa187a975
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-07-16 20:14:38 +0200

    refactor(app-item-list): update props handling and improve display state management

--- apps/stuff-finder/src/components/app-item-list.tsx
+++ apps/stuff-finder/src/components/app-item-list.tsx
@@ -4,16 +4,23 @@ import type { Display } from '../types/theme.types'
 import { state, watchState } from '../utils/state.utils'
 import { AppItemListEntry } from './app-item-list-entry'
 
-export function AppItemList({ items }: Readonly<{ items: Item[] }>) {
-  const [display, setDisplay] = useState<Display>(state.display)
-  watchState('display', () => {
-    setDisplay(state.display)
-  })
+type Props = Readonly<{
+  display?: Display
+  items: Item[]
+}>
+
+export function AppItemList(props: Props) {
+  const [display, setDisplay] = useState<Display>(props.display ?? state.display)
+
+  if (props.display === undefined)
+    watchState('display', () => {
+      setDisplay(state.display)
+    })
 
   return (
     <nav aria-label="item list" class="mb-20 overflow-y-auto overflow-x-hidden md:mb-0" data-component="item-list">
       <div class={`grid grid-cols-1 bg-gray-100 ${display === 'list' ? '' : 'xs:grid-cols-2 gap-3 p-3 sm:grid-cols-3 sm:gap-5 sm:p-5'}`} data-type="list">
-        {items.map(item => (
+        {props.items.map(item => (
           <AppItemListEntry display={display} item={item} key={item.$id} />
         ))}
       </div>
