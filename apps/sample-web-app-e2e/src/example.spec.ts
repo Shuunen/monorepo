aa0f96976baa1d8b7c31a55903872be7881e25ce
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-07-24 13:58:16 +0200

    feat(sample-web-app): use monorepo components

--- apps/sample-web-app-e2e/src/example.spec.ts
+++ apps/sample-web-app-e2e/src/example.spec.ts
@@ -3,6 +3,6 @@ import { expect, test } from '@playwright/test'
 test('has title', async ({ page }) => {
   await page.goto('/')
 
-  // Expect h1 to contain a substring.
-  expect(await page.locator('h2').textContent()).toContain('Welcome')
+  // Expect title to contain a specific string
+  expect(await page.locator('h1').textContent()).toContain('Sample Web App')
 })
