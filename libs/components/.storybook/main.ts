07efc73a7b0b57f57d572ba5fb2096d93418edf0
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-07-24 14:51:42 +0200

    feat(components): add storybook 21

---
+++ libs/components/.storybook/main.ts
@@ -0,0 +1,22 @@
+import type { StorybookConfig } from '@storybook/react-vite'
+
+const config: StorybookConfig = {
+  addons: [],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {
+      builder: {
+        viteConfigPath: 'vite.config.ts',
+      },
+    },
+  },
+  // initially : '../src/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'
+  stories: ['../src/**/*.@(mdx|stories.tsx)'],
+}
+
+// oxlint-disable-next-line no-default-export
+export default config
+
+// To customize your Vite configuration you can use the viteFinal field.
+// Check https://storybook.js.org/docs/react/builders/vite#configuration
+// and https://nx.dev/recipes/storybook/custom-builder-configs
---
+++ libs/components/src/molecules/landing.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { expect } from 'storybook/test'
+import { Landing } from './landing'
+
+const meta: Meta<typeof Landing> = {
+  component: Landing,
+  title: 'Landing',
+}
+export default meta
+type Story = StoryObj<typeof Landing>
+
+export const Basic: Story = {
+  args: {
+    status: 'the status text',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: 'Welcome to Landing',
+  },
+  play: async ({ canvas }) => {
+    await expect(canvas.getByText(/Welcome to Landing!/gi)).toBeTruthy()
+  },
+}
