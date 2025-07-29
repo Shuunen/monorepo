a749654c9099ab3e5b2f1bb008225528392f18c0
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-07-29 15:57:53 +0200

    feat(components): add Storybook tests

--- libs/components/.storybook/preview.ts
+++ libs/components/.storybook/preview.ts
@@ -1,1 +1,20 @@
+import type { Preview } from '@storybook/react-vite'
+// Import Tailwind CSS styles for Storybook
+// oxlint-disable-next-line no-unassigned-import
+import '../src/styles.css'
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+}
+
 console.log('Storybook preview configuration loaded')
+
+// oxlint-disable-next-line no-default-export
+export { preview as default }
