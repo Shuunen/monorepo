4bda19f4bb3bf8b7019cacbae8e545443b995d58
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-07-29 19:58:30 +0200

    fix(types): add missing semicolons in type definitions

--- libs/vite-plugins/types/lib/unique-mark.node.d.ts
+++ libs/vite-plugins/types/lib/unique-mark.node.d.ts
@@ -6,7 +6,7 @@
  * @param {string} mark the mark to inject
  * @returns the new string with the mark injected
  */
-export function injectMark(content: string, placeholder: string, mark: string): string
+export function injectMark(content: string, placeholder: string, mark: string): string;
 /**
  * Generate the mark to inject
  * @param {object} root0 the options
@@ -15,7 +15,11 @@ export function injectMark(content: string, placeholder: string, mark: string): 
  * @param {string} [root0.version] the version to use, if empty, will use the version from package.json
  * @returns the mark to inject, like "4.2.0 - 123abc45 - 01/01/2021 12:00:00"
  */
-export function generateMark({ commit, date, version }: { commit?: string | undefined; date?: string | undefined; version?: string | undefined }): string
+export function generateMark({ commit, date, version }: {
+    commit?: string | undefined;
+    date?: string | undefined;
+    version?: string | undefined;
+}): string;
 /**
  * Injects a unique mark into the provided asset's content at the specified placeholder.
  * @param {object} params - The parameters for injection.
@@ -24,25 +28,32 @@ export function generateMark({ commit, date, version }: { commit?: string | unde
  * @param {string} params.mark - The unique mark to inject.
  * @param {string} params.placeholder - The placeholder string to be replaced by the mark.
  */
-export function injectMarkInAsset({ asset, fileName, mark, placeholder }: { asset: Record<string, string>; fileName: string; mark: string; placeholder: string }): void
+export function injectMarkInAsset({ asset, fileName, mark, placeholder }: {
+    asset: Record<string, string>;
+    fileName: string;
+    mark: string;
+    placeholder: string;
+}): void;
 /**
  * Injects a unique mark into specified asset files (HTML, JS, and CSS) by replacing a placeholder.
  * @param {Record<string, Record<string,string>>} assets - An object mapping file names to asset contents.
  * @param {string} placeholder - The placeholder string to be replaced with the unique mark.
  * @param {string} version - The version string to include in the generated mark.
  */
-export function injectMarkInAssets(assets: Record<string, Record<string, string>>, placeholder: string, version: string): void
+export function injectMarkInAssets(assets: Record<string, Record<string, string>>, placeholder: string, version: string): void;
 /**
  * Retrieves the version of a project from its package.json file.
  * @param {string} projectRoot - The root directory of the project.
  * @returns {string} The version specified in package.json, or an empty string if not found or on error.
  */
-export function getProjectVersion(projectRoot: string): string
+export function getProjectVersion(projectRoot: string): string;
 /**
  * Vite plugin to inject a unique mark (such as a version string) into build assets.
  * @param {object} [options] - Plugin options.
  * @param {string} [options.placeholder] - Placeholder string to be replaced in assets.
  * @returns {import('vite').Plugin} Vite plugin object.
  */
-export function uniqueMark(options?: { placeholder?: string | undefined }): import('vite').Plugin
+export function uniqueMark(options?: {
+    placeholder?: string | undefined;
+}): import("vite").Plugin;
 //# sourceMappingURL=unique-mark.node.d.ts.map
