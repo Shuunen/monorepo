0054b7ee50a7dfd165ae4b66f6f5dc773e974b6a
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-07-29 17:49:38 +0200

    feat(tsconfig): add baseUrl and path mappings for common libraries

--- apps/sample-web-app/src/app/app.tsx
+++ apps/sample-web-app/src/app/app.tsx
@@ -1,9 +1,11 @@
 import { Landing } from '@shuunen/components'
+import { randomPerson } from '@shuunen/shuutils'
 import { NavLink, Route, Routes } from 'react-router-dom'
 
 const navClasses = ({ isActive }: { isActive: boolean }) => `transition-colors ${isActive ? 'text-blue-600 underline underline-offset-22' : 'text-gray-700 hover:text-blue-600'}`
 
 export function App() {
+  const person = randomPerson()
   return (
     <div>
       <nav className="absolute flex gap-6 text-2xl font-semibold justify-center w-full p-4 bg-white shadow-md">
@@ -16,7 +18,7 @@ export function App() {
       </nav>
       <Routes>
         <Route element={<Landing status="Use me as a template" subtitle="Web application built with React and TailwindCSS" title="Sample Web App" />} path="/" />
-        <Route element={<Landing subtitle="Lorem ipsum sit dolor in Shuunen" title="About" />} path="/about" />
+        <Route element={<Landing subtitle={`This project is the great work of ${person.firstName} ${person.lastName} 😎`} title="About" />} path="/about" />
       </Routes>
     </div>
   )
