781b43978776adfd0c37989c6e0b2f9bb55715b1
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-07-31 14:59:37 +0200

    feat(components): add auto listing

--- libs/shuutils/src/list.cli.ts
+++ libs/shuutils/src/list.cli.ts
@@ -2,19 +2,26 @@
 import { writeFileSync } from 'node:fs'
 import path from 'node:path'
 import glob from 'tiny-glob'
+import { green } from './lib/colors.js'
 import { Logger } from './lib/logger.js'
+
+const options = {
+  avoid: '.test.ts',
+  ext: '.js',
+  index: 'index.ts',
+  target: './lib/*.ts',
+}
 
 /**
  * List entries in lib folder into the barrel index.ts file
  * @returns {Promise<void>}
  */
-async function listEntries() {
+export async function listEntries() {
   const logger = new Logger()
-  const lib = path.join(import.meta.dirname, 'lib')
-  const index = path.join(import.meta.dirname, 'index.ts')
-  logger.info('Listing entries in lib folder..., target: ', lib)
-  const files = await glob('*.ts', { cwd: lib, filesOnly: true })
-  const list = files.filter(file => !file.includes('.test.ts')).map(file => `export ${file.includes('types') ? 'type ' : ''}* from './lib/${file.replace('.ts', '.js')}'`)
+  const index = path.join(import.meta.dirname, options.index)
+  logger.info('Listing entries', green(options.target))
+  const files = await glob(options.target, { cwd: import.meta.dirname, filesOnly: true })
+  const list = files.filter(file => !file.includes(options.avoid)).map(file => `export ${file.includes('types') ? 'type ' : ''}* from './${options.ext === undefined ? file : file.split('.')[0] + options.ext}'`)
   const content = `${list.sort().join('\n')}\n`
   writeFileSync(index, content)
   logger.success(`${index} has been updated !`)
