f901aa127bb004732833da69b575fb50ebb8ca1f
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-08-13 19:23:58 +0200

    fix(floating-menu): handle disabled actions

--- libs/components/src/molecules/floating-menu.tsx
+++ libs/components/src/molecules/floating-menu.tsx
@@ -3,14 +3,15 @@ import { createElement, type ForwardRefExoticComponent, type RefAttributes, useE
 import { Command, CommandEmpty, CommandItem, CommandList } from '../shadcn/command'
 import { Popover, PopoverContent, PopoverTrigger } from '../shadcn/popover'
 
-type Actions = {
+export type FloatingMenuAction = {
+  disabled?: boolean
   handleClick: () => void
   icon: ForwardRefExoticComponent<Omit<LucideProps, 'ref'> & RefAttributes<SVGSVGElement>>
   name: string
 }
 
 // oxlint-disable-next-line max-lines-per-function
-export function FloatingMenu({ actions = [], isLoading = false, isSettingsRequired = false }: Readonly<{ actions: Actions[]; isLoading?: boolean; isSettingsRequired?: boolean }>) {
+export function FloatingMenu({ actions = [], isLoading = false, isSettingsRequired = false }: Readonly<{ actions: FloatingMenuAction[]; isLoading?: boolean; isSettingsRequired?: boolean }>) {
   const [isOpen, setOpen] = useState(false)
   const iconOpen = isOpen ? <PackageOpenIcon /> : <PackageIcon />
   const icon = useMemo(() => (isLoading ? <LoaderCircleIcon /> : iconOpen), [iconOpen, isLoading])
@@ -24,13 +25,13 @@ export function FloatingMenu({ actions = [], isLoading = false, isSettingsRequir
       {isOpen && <div className="absolute fixed bottom-0 right-0 z-10 size-full bg-linear-to-tl bg-black/20" data-component="speed-dial-backdrop" />}
       <Popover onOpenChange={setOpen}>
         <PopoverTrigger className="cursor-pointer transition-all opacity-50 hover:opacity-100 bottom-5 right-5 fixed bg-secondary-foreground p-4 rounded-full">{icon}</PopoverTrigger>
-        <PopoverContent className="p-1 mr-5 mb-2">
+        <PopoverContent className="p-1 mr-5 mb-2 w-fit">
           <Command>
             <CommandList>
               <CommandEmpty>No results found.</CommandEmpty>
               {/* <CommandItem>Profile</CommandItem> */}
               {availableActions.map(action => (
-                <CommandItem className="cursor-pointer text-lg" key={action.name} onSelect={action.handleClick}>
+                <CommandItem className="cursor-pointer text-lg" disabled={action.disabled} key={action.name} onSelect={action.handleClick}>
                   {createElement(action.icon, { className: 'size-5' })}
                   {action.name}
                 </CommandItem>
