990d150438ea507e06175ac151074836c493c777
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-09-22 13:25:28 +0200

    feat(dev-script): add user script for auto-reload functionality

---
+++ apps/user-scripts/src/dev.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Dev script - To have some kind of auto-reload
+// @author       Romain Racamier-Lafon
+// @description  Let's you play with your scripts
+// @grant        none
+// @match        https://www.__HEY_CHANGE_ME__.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @namespace    https://github.com/Shuunen
+// @require      file://C:\Users\__HEY_CHANGE_ME__\Projects\github\monorepo\apps\user-scripts\src\utils.js
+// @require      file://C:\Users\__HEY_CHANGE_ME__\Projects\github\monorepo\apps\user-scripts\src\hdd-cleaner.user.js
+// @version      2.2.2
+// ==/UserScript==
+
+// oxlint-disable no-console
+// biome-ignore lint/suspicious/noConsole: it's ok here
+console.log('hello from dev script, loading __HEY_CHANGE_ME__')
