e2226a419b5f15be838bd4019da241d03f6baa90
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-10-01 15:54:42 +0200

    chore: add vet-web

---
+++ apps/vet-web/src/app/app.tsx
@@ -0,0 +1,25 @@
+import { Landing } from '@monorepo/components'
+import { randomPerson } from '@monorepo/utils'
+import { NavLink, Route, Routes } from 'react-router-dom'
+
+const navClasses = ({ isActive }: { isActive: boolean }) => `transition-colors ${isActive ? 'text-primary underline underline-offset-22' : 'text-gray-700 hover:text-primary'}`
+
+export function App() {
+  const person = randomPerson()
+  return (
+    <div>
+      <nav className="absolute flex gap-6 text-2xl font-semibold justify-center w-full p-4 bg-white shadow-md">
+        <NavLink className={navClasses} to="/">
+          Home
+        </NavLink>
+        <NavLink className={navClasses} to="/about">
+          About
+        </NavLink>
+      </nav>
+      <Routes>
+        <Route element={<Landing status="I'm a starter base project" subtitle="Web application built with React and TailwindCSS" title="vet-web" />} path="/" />
+        <Route element={<Landing subtitle={`This project is the great work of ${person.firstName} ${person.lastName} 😎`} title="About" />} path="/about" />
+      </Routes>
+    </div>
+  )
+}
