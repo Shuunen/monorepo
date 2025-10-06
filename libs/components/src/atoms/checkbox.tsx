e101d4be3ae8574c0ce975c77e68f0a4a6c22ce6
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-10-06 12:40:52 +0200

    feat: implement Checkbox component with dynamic testId generation

--- libs/components/src/atoms/checkbox.tsx
+++ libs/components/src/atoms/checkbox.tsx
@@ -1,1 +1,12 @@
-export { Checkbox } from '../shadcn/checkbox'
+import { camelToKebabCase, slugify } from '@monorepo/utils'
+import type { ComponentProps } from 'react'
+import { Checkbox as ShadCheckbox } from '../shadcn/checkbox'
+
+type CheckboxProps = ComponentProps<typeof ShadCheckbox> & {
+  testId?: string
+}
+
+export function Checkbox({ ...props }: CheckboxProps) {
+  const testId = props.testId || slugify(camelToKebabCase(props.name || 'checkbox'))
+  return <ShadCheckbox data-testid={testId} {...props} />
+}
