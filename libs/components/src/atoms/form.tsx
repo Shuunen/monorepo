a916e68363c05255f3367cb40d2807b20208aed9
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-10-06 12:40:59 +0200

    feat: add FormMessage component with testId prop for accessibility

--- libs/components/src/atoms/form.tsx
+++ libs/components/src/atoms/form.tsx
@@ -1,1 +1,12 @@
-export { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '../shadcn/form'
+import type { ComponentProps } from 'react'
+import { FormMessage as ShadFormMessage } from '../shadcn/form'
+
+export { Form, FormControl, FormDescription, FormField, FormItem, FormLabel } from '../shadcn/form'
+
+type FormMessageProps = ComponentProps<typeof ShadFormMessage> & {
+  testId: string
+}
+
+export function FormMessage({ ...props }: FormMessageProps) {
+  return <ShadFormMessage data-testid={props.testId} {...props} role="alert" />
+}
