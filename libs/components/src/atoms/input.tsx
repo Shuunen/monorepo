4a9a7258c3f37305bc208bd157c3d7ee6a7af581
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-10-06 12:41:07 +0200

    feat: make testId prop optional in Input component and enhance dynamic generation

--- libs/components/src/atoms/input.tsx
+++ libs/components/src/atoms/input.tsx
@@ -1,10 +1,12 @@
+import { camelToKebabCase, slugify } from '@monorepo/utils'
 import type { ComponentProps } from 'react'
 import { Input as ShadInput } from '../shadcn/input'
 
 type InputProps = ComponentProps<typeof ShadInput> & {
-  testId: string
+  testId?: string
 }
 
 export function Input(props: InputProps) {
-  return <ShadInput data-testid={props.testId} {...props} />
+  const testId = props.testId || slugify(camelToKebabCase(props.name || 'checkbox'))
+  return <ShadInput data-testid={testId} {...props} />
 }
