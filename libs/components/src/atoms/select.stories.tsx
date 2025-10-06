a347233e8d1de1d27e3264fce8a67dde7b43bdae
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-10-07 01:25:13 +0200

    feat: enhance Select component and stories

--- libs/components/src/atoms/select.stories.tsx
+++ libs/components/src/atoms/select.stories.tsx
@@ -1,10 +1,23 @@
+// Replace nextjs-vite with the name of your framework
 import type { Meta, StoryObj } from '@storybook/react-vite'
-import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from './select'
+import { expect, fn, userEvent, within } from 'storybook/test'
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectSeparator, SelectTrigger, SelectValue } from './select'
 
-const meta = {
-  component: () => (
-    <Select>
-      <SelectTrigger className="w-[180px]">
+/**
+ * Displays a list of options for the user to pick from—triggered by a button.
+ */
+const meta: Meta<typeof Select> = {
+  args: {
+    onValueChange: fn(),
+  },
+  argTypes: {},
+  component: Select,
+  parameters: {
+    layout: 'centered',
+  },
+  render: args => (
+    <Select {...args}>
+      <SelectTrigger className="w-96" title="Select">
         <SelectValue placeholder="Select a fruit" />
       </SelectTrigger>
       <SelectContent>
@@ -16,12 +29,28 @@ const meta = {
           <SelectItem value="grapes">Grapes</SelectItem>
           <SelectItem value="pineapple">Pineapple</SelectItem>
         </SelectGroup>
+        <SelectSeparator />
+        <SelectGroup>
+          <SelectLabel>Vegetables</SelectLabel>
+          <SelectItem value="aubergine">Aubergine</SelectItem>
+          <SelectItem value="broccoli">Broccoli</SelectItem>
+          <SelectItem disabled value="carrot">
+            Carrot
+          </SelectItem>
+          <SelectItem value="courgette">Courgette</SelectItem>
+          <SelectItem value="leek">Leek</SelectItem>
+        </SelectGroup>
+        <SelectSeparator />
+        <SelectGroup>
+          <SelectLabel>Meat</SelectLabel>
+          <SelectItem value="beef">Beef</SelectItem>
+          <SelectItem value="chicken">Chicken</SelectItem>
+          <SelectItem value="lamb">Lamb</SelectItem>
+          <SelectItem value="pork">Pork</SelectItem>
+        </SelectGroup>
       </SelectContent>
     </Select>
   ),
-  parameters: {
-    layout: 'centered',
-  },
   tags: ['autodocs'],
   title: 'atoms/Select',
 } satisfies Meta<typeof Select>
@@ -30,6 +59,29 @@ export default meta
 
 type Story = StoryObj<typeof meta>
 
-export const Simple: Story = {
-  args: {},
+/**
+ * The default form of the select.
+ */
+export const Default: Story = {}
+
+export const ShouldSelectOption: Story = {
+  name: 'when an option is selected, should be checked',
+  play: async ({ canvasElement, step }) => {
+    const canvasBody = within(canvasElement.ownerDocument.body)
+    const select = await canvasBody.findByRole('combobox')
+
+    await step('open and select item', async () => {
+      await userEvent.click(select)
+      const options = canvasBody.getAllByRole('option')
+      await userEvent.click(options[1])
+      expect(select).toHaveTextContent('Banana')
+    })
+
+    await step('verify the selected option', async () => {
+      await userEvent.click(select)
+      const options = canvasBody.getAllByRole('option')
+      expect(options[1]).toHaveAttribute('data-state', 'checked')
+      await userEvent.click(options[1])
+    })
+  },
 }
--- libs/components/src/atoms/select.tsx
+++ libs/components/src/atoms/select.tsx
@@ -1,1 +1,23 @@
-export { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from '../shadcn/select'
+import { camelToKebabCase, slugify } from '@monorepo/utils'
+import type { ComponentProps } from 'react'
+import { Select as ShadcnSelect, SelectTrigger as ShadSelectTrigger } from '../shadcn/select'
+
+type SelectProps = ComponentProps<typeof ShadcnSelect> & {
+  testId?: string
+}
+
+export function Select(props: SelectProps) {
+  const testId = props.testId || slugify(camelToKebabCase(props.name || 'select'))
+  return <ShadcnSelect data-testid={testId} {...props} />
+}
+
+type SelectTriggerProps = ComponentProps<typeof ShadSelectTrigger> & {
+  testId?: string
+}
+
+export function SelectTrigger(props: SelectTriggerProps) {
+  const testId = props.testId || slugify(camelToKebabCase(props.name || 'select-trigger'))
+  return <ShadSelectTrigger data-testid={testId} {...props} />
+}
+
+export { SelectContent, SelectGroup, SelectItem, SelectLabel, SelectSeparator, SelectValue } from '../shadcn/select'
