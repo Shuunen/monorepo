7b641b555ff2832d10f3a54ddd77111e98db33cd
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-10-12 10:43:09 +0200

    fix(alignForSnap): improve regex for date-time formatting

--- libs/utils/src/lib/testing-align-for-snap.ts
+++ libs/utils/src/lib/testing-align-for-snap.ts
@@ -16,7 +16,7 @@ export function alignForSnap(content: unknown) {
       // 6/25/2024, 11:21:23 AM => xx/xx/xxxx xx:xx:xx
       .replace(/\d{1,2}\/\d{1,2}\/\d{4}, \d{1,2}:\d{2}:\d{2} [ap]m/giu, 'xx/xx/xxxx xx:xx:xx')
       // 2024-05-16T15:36:32.000Z => xxxx-xx-xxTxx:xx:xx.000Z
-      .replace(/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z/gu, 'xxxx-xx-xxTxx:xx:xx.000Z')
+      .replace(/\d{4}-\d{2}-\d{2}([T ])\d{2}:\d{2}:\d{2}(\.\d{3}Z)?/gu, 'xxxx-xx-xx$1xx:xx:xx$2')
       // \\documents\\file.pdf => /documents/file.pdf
       .replace(/\\+(?<letter>[^"])/gu, '/$<letter>')
   )
