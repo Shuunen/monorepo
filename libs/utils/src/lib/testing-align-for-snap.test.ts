c9c7eb44df6ba9504700b7e9b0929c21613799d0
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-10-12 17:42:58 +0200

    fix(alignForSnap): update date-time formatting to include milliseconds in snapshots

--- libs/utils/src/lib/testing-align-for-snap.test.ts
+++ libs/utils/src/lib/testing-align-for-snap.test.ts
@@ -1,6 +1,6 @@
 import { alignForSnap } from './testing-align-for-snap.js'
 
-it('cleanForSnap A - Windows filepath', () => {
+it('cleanForSnap A Windows filepath', () => {
   const result = alignForSnap({ value: String.raw`\documents\my-file.pdf` })
   expect(result).toMatchInlineSnapshot(`
     "{
@@ -9,16 +9,24 @@ it('cleanForSnap A - Windows filepath', () => {
   `)
 })
 
-it('cleanForSnap B - French date', () => {
+it('cleanForSnap B French date', () => {
   const result = alignForSnap('Lu et approuvé le 16/05/2024 17:36:32')
   expect(result).toMatchInlineSnapshot('"Lu et approuvé le xx/xx/xxxx xx:xx:xx"')
 })
 
-it('cleanForSnap C - American date', () => {
+it('cleanForSnap C American date', () => {
   const result = alignForSnap('Read & approved on 6/25/2024, 11:21:23 AM')
   expect(result).toMatchInlineSnapshot('"Read & approved on xx/xx/xxxx xx:xx:xx"')
 })
 
-it('cleanForSnap D - Date instance', () => {
-  expect(alignForSnap(new Date('2024-05-16T17:36:32'))).toMatchInlineSnapshot('""xxxx-xx-xxTxx:xx:xx.000Z""')
+it('cleanForSnap D Date instance', () => {
+  expect(alignForSnap(new Date('2024-05-16T17:36:32'))).toMatchInlineSnapshot('""xxxx-xx-xxTxx:xx:xx.xxxZ""')
+})
+
+it('cleanForSnap E kind of ISO date string', () => {
+  expect(alignForSnap('2024-05-16 15:36:32.000Z')).toMatchInlineSnapshot('"xxxx-xx-xx xx:xx:xx.xxxZ"')
+})
+
+it('cleanForSnap F kind of ISO date string without timezone', () => {
+  expect(alignForSnap('2024-05-16T15:36:32')).toMatchInlineSnapshot('"xxxx-xx-xxTxx:xx:xx"')
 })
