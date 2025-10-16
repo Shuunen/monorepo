419d39cb66eb2b394e8c040e420fd021bb0458be
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-10-16 12:31:05 +0200

    chore: move barrel maker to shared libs

--- libs/utils/src/bin/barrel-maker.cli.test.ts
+++ libs/utils/src/bin/barrel-maker.cli.test.ts
@@ -11,40 +11,48 @@ describe('barrel-maker cli', () => {
   })
 
   it('make A ext empty and default index.ts', async () => {
+    // @ts-expect-error import issue
     const { make } = await import('./barrel-maker.cli')
     const { value } = await make({ avoid: '.stories.', ext: '', target: './{atoms,icons,molecules}/*.tsx' })
     expect(value.content).toMatchSnapshot()
     expect(value.files).toMatchSnapshot()
-    expect(alignForSnap(value.out)).toContain(`apps/one-file/index.ts`)
+    expect(alignForSnap(value.out)).toContain(`libs/utils/index.ts`)
   })
 
   it('make B index.js and ext .js', async () => {
+    // @ts-expect-error import issue
     const { make } = await import('./barrel-maker.cli')
     const { value } = await make({ avoid: '.test.ts', ext: '.js', index: 'index.js', target: './lib/*.ts' })
     expect(value.content).toMatchSnapshot()
     expect(value.files).toMatchSnapshot()
-    expect(alignForSnap(value.out)).toContain(`apps/one-file/index.js`)
+    expect(alignForSnap(value.out)).toContain(`libs/utils/index.js`)
   })
 
   it('make C undefined ext', async () => {
+    // @ts-expect-error import issue
     const { make } = await import('./barrel-maker.cli')
     const { value } = await make({ avoid: '.stories.', ext: undefined, target: './{atoms,icons,molecules}/*.tsx' })
     expect(value.content).toMatchSnapshot()
     expect(value.files).toMatchSnapshot()
-    expect(alignForSnap(value.out)).toContain(`apps/one-file/index.ts`)
+    expect(alignForSnap(value.out)).toContain(`libs/utils/index.ts`)
   })
 
   it('main A empty args', async () => {
+    // @ts-expect-error import issue
     const { main } = await import('./barrel-maker.cli')
     const { error } = Result.unwrap(await main(['node', 'script.ts']))
     expect(error).toMatchInlineSnapshot(`"missing target argument"`)
   })
 
   it('main B with args', async () => {
+    // @ts-expect-error import issue
     const { main } = await import('./barrel-maker.cli')
     const { value } = Result.unwrap(await main(['node', 'script.ts', '--target=./{atoms,icons,molecules}/*.tsx', '--avoid=.stories.', '--ext=.js', '--index=index.js']))
+    // @ts-expect-error import issue
     expect(value?.content).toMatchSnapshot()
+    // @ts-expect-error import issue
     expect(value?.files).toMatchSnapshot()
-    expect(alignForSnap(value?.out)).toContain(`apps/one-file/index.js`)
+    // @ts-expect-error import issue
+    expect(alignForSnap(value?.out)).toContain(`libs/utils/index.js`)
   })
 })
