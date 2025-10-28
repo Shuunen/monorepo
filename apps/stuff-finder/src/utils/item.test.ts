19a71690632f7f54723ef21ec7d70aa707192f4c
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-10-28 16:57:31 +0100

    feat: register GlobalRegistrator in item test file for improved environment setup

--- apps/stuff-finder/src/utils/item.test.ts
+++ apps/stuff-finder/src/utils/item.test.ts
@@ -1,10 +1,13 @@
 // oxlint-disable max-lines
+import { GlobalRegistrator } from '@happy-dom/global-registrator'
 import { alignForSnap, clone, nbMsInMinute, Result, sleep } from '@monorepo/utils'
 import { databaseMock } from './database.mock'
 import { removeAppWriteFields } from './database.utils'
 import { addItem, areItemsEquivalent, boxStringToBox, deleteItem, drawerStringToDrawer, formToItem, getItems, isDataOlderThan, itemBoxToRoom, itemForm, itemToForm, itemToLocation, statusStringToStatus, updateItem } from './item.utils'
 import { logger } from './logger.utils'
 import { mockFetch, mockItem, mockState } from './mock.utils'
+
+if (!GlobalRegistrator.isRegistered) GlobalRegistrator.register()
 
 vi.mock('appwrite', () => databaseMock.appwrite)
 
