a47b3185971ef7441111eb2fd6c9b1ba60715ce4
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-11-17 15:13:07 +0100

    feat: improve auto form stepper visual

--- libs/components/src/icons/icon-readonly.tsx
+++ libs/components/src/icons/icon-readonly.tsx
@@ -1,5 +1,5 @@
 import { EyeIcon } from 'lucide-react'
 
-export function IconReadonly() {
-  return <EyeIcon />
+export function IconReadonly({ className }: { className?: string }) {
+  return <EyeIcon className={className} />
 }
--- libs/components/src/molecules/auto-form-stepper.tsx
+++ libs/components/src/molecules/auto-form-stepper.tsx
@@ -1,8 +1,11 @@
-import { slugify } from '@monorepo/utils'
+import { cn, slugify } from '@monorepo/utils'
 import { Button } from '../atoms/button'
 
 type AutoFormStepperStep = {
-  label: string
+  title: string
+  subtitle?: string
+  suffix?: string
+  indent?: boolean
   icon: React.ReactNode
   active: boolean
   idx: number
@@ -18,10 +21,24 @@ type AutoFormStepperProps = {
 export function AutoFormStepper({ steps, onStepClick, disabled = false }: AutoFormStepperProps) {
   return (
     <div className="flex flex-col gap-4 pr-8 border-r border-gray-200 mr-8">
-      {steps.map(({ label, icon, active, idx, state }) => (
-        <Button data-state={state} disabled={disabled} key={label} onClick={() => onStepClick(idx)} testId={`step-${slugify(label)}`} variant={active ? 'default' : 'ghost'}>
+      {steps.map(({ title, subtitle, suffix, icon, active, idx, state }) => (
+        <Button
+          className={cn('h-10', { 'h-16 rounded-xl': subtitle }, { 'bg-transparent text-black border border-gray-500 hover:bg-gray-100': active })}
+          data-state={state}
+          disabled={disabled}
+          key={title}
+          onClick={() => onStepClick(idx)}
+          testId={`step-${slugify(title)}`}
+          variant={active ? 'default' : 'ghost'}
+        >
           {icon}
-          <span className="grow text-start">{label}</span>
+          <div className="grow text-start flex flex-col ml-2">
+            <div className="flex items-center gap-2">
+              <span>{title}</span>
+              {suffix && <span className="text-xs text-muted-foreground">{suffix}</span>}
+            </div>
+            {subtitle && <span className="text-xs text-muted-foreground">{subtitle}</span>}
+          </div>
         </Button>
       ))}
     </div>
