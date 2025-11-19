da7b4a41cc1906d86316d4f869b9c628bae86e0b
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-11-19 13:26:57 +0100

    feat: add password field support to AutoForm and related components

---
+++ libs/components/src/molecules/form-field-password.tsx
@@ -0,0 +1,21 @@
+import { FormControl } from '../atoms/form'
+import { Input } from '../atoms/input'
+import { getFieldMetadata } from './auto-form.utils'
+import { FormFieldBase, type FormFieldBaseProps } from './form-field'
+
+export function FormFieldPassword({ fieldName, fieldSchema, formData, isOptional, logger, readonly = false }: FormFieldBaseProps) {
+  const metadata = getFieldMetadata(fieldSchema)
+  if (!metadata) throw new Error(`Field "${fieldName}" is missing metadata (label, placeholder, state)`)
+  const { placeholder, state = 'editable' } = metadata
+  const isDisabled = state === 'disabled'
+  const props = { fieldName, fieldSchema, formData, isOptional, logger, readonly }
+  return (
+    <FormFieldBase {...props}>
+      {field => (
+        <FormControl>
+          <Input {...field} disabled={isDisabled} placeholder={placeholder} readOnly={readonly} type="password" />
+        </FormControl>
+      )}
+    </FormFieldBase>
+  )
+}
