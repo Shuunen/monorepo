bc02495fce67149d8bd788cf97b01a2dc1aca3de
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-11-19 17:25:31 +0100

    feat: implement Badge component with customizable variants and data-testid support

--- libs/components/src/atoms/badge.tsx
+++ libs/components/src/atoms/badge.tsx
@@ -1,3 +1,36 @@
-export { Badge } from '../shadcn/badge'
+import { cva, type VariantProps } from 'class-variance-authority'
+import type { ComponentProps } from 'react'
+import { Badge as ShadBadge } from '../shadcn/badge'
+import { cn } from '../shadcn/utils'
 
-// TODO : ideally we add data-testid handling (and other customizations) like in button.tsx instead of just exposing the raw shadcn component
+type BadgeProps = ComponentProps<typeof ShadBadge> &
+  VariantProps<typeof badgeVariants> & {
+    asChild?: boolean
+    testId?: string
+  }
+
+const badgeVariants = cva(
+  "inline-flex items-center justify-center rounded-full border px-2 py-0.5 text-xs font-medium w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden",
+  {
+    defaultVariants: {
+      variant: 'default',
+    },
+    variants: {
+      variant: {
+        default: 'border-transparent bg-primary text-primary-foreground [a&]:hover:bg-primary/90',
+        destructive: 'border-transparent bg-destructive text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
+        outline: 'text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground',
+        secondary: 'border-transparent bg-secondary text-secondary-foreground [a&]:hover:bg-secondary/90',
+        warning: 'border-transparent bg-warning text-white [a&]:hover:bg-warning/90 focus-visible:ring-warning/20 dark:focus-visible:ring-warning/40 dark:bg-warning/60',
+      },
+    },
+  },
+)
+
+export function Badge({ children, variant, className = '', asChild = false, testId, ...props }: BadgeProps) {
+  return (
+    <ShadBadge asChild={asChild} className={cn(badgeVariants({ className, variant }))} data-testid={testId} variant={variant} {...props}>
+      {children}
+    </ShadBadge>
+  )
+}
