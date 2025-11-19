e16d1842f9d7ef82fea0befa8b8d1b4f2ffa27a7
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-11-19 17:13:57 +0100

    refactor: remove deprecated zodMsg function and update tests to use zodSnap

--- libs/utils/src/lib/zod.ts
+++ libs/utils/src/lib/zod.ts
@@ -1,16 +1,6 @@
 import type { ZodError } from 'zod'
 
 /**
- * Get the first message from a zod error
- * @param error the zod error to extract the message from
- * @deprecated use zodSnap instead because this function only returns the first issue
- * @returns the first message
- */
-export function zodMsg(error?: ZodError) {
-  return error?.issues[0].message
-}
-
-/**
  * Format a zod error to be short and readable in a snapshot
  * @param error the zod error to format
  * @returns the formatted error
