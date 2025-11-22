2634906342156825c2421dbe1c88c3463d864ea8
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-11-22 11:23:03 +0100

    feat: fix tsconfigs

--- libs/components/src/atoms/checkbox.stories.tsx
+++ libs/components/src/atoms/checkbox.stories.tsx
@@ -20,18 +20,22 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-  args: {},
+  args: {
+    name: 'default',
+  },
 }
 
 export const Checked: Story = {
   args: {
     defaultChecked: true,
+    name: 'checked',
   },
 }
 
 export const Disabled: Story = {
   args: {
     disabled: true,
+    name: 'disabled',
   },
 }
 
@@ -39,15 +43,19 @@ export const DisabledChecked: Story = {
   args: {
     defaultChecked: true,
     disabled: true,
+    name: 'disabled-checked',
   },
 }
 
 export const WithLabel: Story = {
+  args: {
+    name: 'with-label',
+  },
   render: () => {
     const id = useId()
     return (
       <div className="flex items-center space-x-2">
-        <Checkbox id={id} />
+        <Checkbox id={id} name="with-label" />
         <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70" htmlFor={id}>
           Accept terms and conditions
         </label>
