99bd345120862af3e1d35223e7a61d460d0ff268
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-12-08 22:27:25 +0100

    feat: update Capacitor config to include Android logging behavior and server scheme

--- apps/what-now/capacitor.config.ts
+++ apps/what-now/capacitor.config.ts
@@ -1,8 +1,14 @@
 import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
+  android: {
+    loggingBehavior: 'debug',
+  },
   appId: 'shuunen.whatnow.app',
   appName: 'what-now',
+  server: {
+    androidScheme: 'http',
+  },
   webDir: 'dist',
 }
 
