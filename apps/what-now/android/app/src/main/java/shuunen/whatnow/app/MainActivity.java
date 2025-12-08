891303ea662be97646bd8a54134e32b9f296432b
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-12-08 22:27:40 +0100

    feat: enable WebView debugging for console logs in MainActivity

--- apps/what-now/android/app/src/main/java/shuunen/whatnow/app/MainActivity.java
+++ apps/what-now/android/app/src/main/java/shuunen/whatnow/app/MainActivity.java
@@ -3,12 +3,15 @@ package shuunen.whatnow.app;
 import android.os.Bundle;
 import android.view.WindowInsets;
 import android.view.WindowInsetsController;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		// Enable WebView debugging for console logs
+		WebView.setWebContentsDebuggingEnabled(true);
 		// Hide the system status bar for fullscreen
 		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
 			final WindowInsetsController insetsController = getWindow().getInsetsController();
