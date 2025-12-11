b2b4fe445b45e7c4c31ae6e019cca6239c05771d
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-12-11 17:35:26 +0100

    feat: add number schema validation functions for improved input handling

--- libs/utils/src/lib/zod.test.ts
+++ libs/utils/src/lib/zod.test.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod'
-import { zodSnap } from './zod.js'
+import { numberSchema, optionalNumberSchema, zodSnap } from './zod.js'
 
 describe('zod', () => {
   it('zodSnap A invalid email', () => {
@@ -18,5 +18,83 @@ describe('zod', () => {
     const result = schema.safeParse('mr-clean@yopmail.fr')
     const message = zodSnap(result.error)
     expect(message).toMatchInlineSnapshot(`undefined`)
+  })
+})
+
+describe('numberSchema', () => {
+  it('empty options, with valid string', () => {
+    const schema = numberSchema()
+    expect(schema.safeParse('12').success).toBe(true)
+  })
+
+  it('min only with valid string in range ', () => {
+    const schema = numberSchema({ min: 1 })
+    expect(schema.safeParse('12').success).toBe(true)
+  })
+
+  it('min only with valid string outside range ', () => {
+    const schema = numberSchema({ min: 1 })
+    expect(schema.safeParse('-1').success).toBe(false)
+  })
+  it('max only with valid string in range ', () => {
+    const schema = numberSchema({ max: 20 })
+    expect(schema.safeParse('12').success).toBe(true)
+  })
+  it('max only with valid string outside range ', () => {
+    const schema = numberSchema({ max: 20 })
+    expect(schema.safeParse('21').success).toBe(false)
+  })
+  it('min/max only with valid string in range ', () => {
+    const schema = numberSchema({ max: 20, min: 10 })
+    expect(schema.safeParse('12').success).toBe(true)
+  })
+  it('min/max only with valid string outside range ', () => {
+    const schema = numberSchema({ max: 20, min: 10 })
+    expect(schema.safeParse('2').success).toBe(false)
+  })
+  it('empty with invalid string  ', () => {
+    const schema = numberSchema()
+    expect(schema.safeParse('leet').success).toBe(false)
+  })
+})
+
+describe('optionalNumberSchema', () => {
+  it('undefined, with valid string', () => {
+    const schema = optionalNumberSchema()
+    expect(schema.safeParse(undefined).success).toBe(true)
+  })
+  it('empty string, with valid string', () => {
+    const schema = optionalNumberSchema()
+    expect(schema.safeParse('').success).toBe(true)
+  })
+
+  it('min only with valid string in range ', () => {
+    const schema = optionalNumberSchema({ min: 1 })
+    expect(schema.safeParse('12').success).toBe(true)
+  })
+
+  it('min only with valid string outside range ', () => {
+    const schema = optionalNumberSchema({ min: 1 })
+    expect(schema.safeParse('-1').success).toBe(false)
+  })
+  it('max only with valid string in range ', () => {
+    const schema = numberSchema({ max: 20 })
+    expect(schema.safeParse('12').success).toBe(true)
+  })
+  it('max only with valid string outside range ', () => {
+    const schema = optionalNumberSchema({ max: 20 })
+    expect(schema.safeParse('21').success).toBe(false)
+  })
+  it('min/max only with valid string in range ', () => {
+    const schema = numberSchema({ max: 20, min: 10 })
+    expect(schema.safeParse('12').success).toBe(true)
+  })
+  it('min/max only with valid string outside range ', () => {
+    const schema = numberSchema({ max: 20, min: 10 })
+    expect(schema.safeParse('2').success).toBe(false)
+  })
+  it('empty with invalid string  ', () => {
+    const schema = optionalNumberSchema()
+    expect(schema.safeParse('leet').success).toBe(false)
   })
 })
--- libs/utils/src/lib/zod.ts
+++ libs/utils/src/lib/zod.ts
@@ -1,4 +1,4 @@
-import type { ZodError } from 'zod'
+import { type ZodError, z } from 'zod'
 
 /**
  * Format a zod error to be short and readable in a snapshot
@@ -7,4 +7,48 @@ import type { ZodError } from 'zod'
  */
 export function zodSnap(error?: ZodError) {
   return error?.issues.map(issue => `${issue.path.join('.')} | ${issue.code} | ${issue.message}`)
+}
+
+type NumberSchemaOptions = {
+  min?: number
+  max?: number
+}
+
+const integerRegex = /^-?\d+$/
+
+export function numberSchema(options?: NumberSchemaOptions) {
+  let schema = z
+    .string({ message: 'This field is required' })
+    .trim()
+    .min(1, { message: 'This field is required' })
+    .refine(value => integerRegex.test(value), {
+      message: 'Should be a number',
+    })
+    .transform(value => Number.parseInt(value, 10))
+
+  const min = options?.min
+  if (min) schema = schema.refine(value => value >= min, { message: `Minimum value is ${min}` })
+
+  const max = options?.max
+  if (max) schema = schema.refine(value => value <= max, { message: `Minimum value is ${max}` })
+
+  return schema
+}
+
+export function optionalNumberSchema(options?: NumberSchemaOptions) {
+  const { min, max } = options ?? {}
+  let schema = z
+    .string()
+    .trim()
+    // "" → undefined, or keep string value
+    .transform(value => (value === '' ? undefined : value))
+    // check if it match the pattern
+    .refine(value => value === undefined || integerRegex.test(value), { message: 'Should be a number' })
+    // convert to number
+    .transform(value => (value === undefined ? undefined : Number.parseInt(value, 10)))
+  if (min !== undefined) schema = schema.refine(value => value === undefined || value >= min, { message: `Minimum value is ${min}` })
+
+  if (max !== undefined) schema = schema.refine(value => value === undefined || value <= max, { message: `Maximum value is ${max}` })
+
+  return schema.optional()
 }
