815c4cd3ea2c6186ff1f7716e75bbc4891218098
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-12-11 17:25:16 +0100

    fix: replace getFieldMetadata with getFieldMetadataOrThrow for improved error handling and update FormFieldBase rendering logic

--- libs/components/src/molecules/form-field.tsx
+++ libs/components/src/molecules/form-field.tsx
@@ -1,8 +1,11 @@
 import type { Logger } from '@monorepo/utils'
 import type { ControllerRenderProps } from 'react-hook-form'
 import type { z } from 'zod'
-import { FormField, FormItem, FormLabel, FormMessage } from '../atoms/form'
-import { getFieldMetadata } from './auto-form.utils'
+import { FormField, FormItem, FormMessage } from '../atoms/form'
+import { cn } from '../shadcn/utils'
+import { getFieldMetadataOrThrow } from './auto-form.utils'
+import './form-field.css'
+import { FormFieldLabel } from './form-field.utils'
 
 export type FormFieldBaseProps = {
   children?: (field: ControllerRenderProps) => React.ReactNode
@@ -12,24 +15,25 @@ export type FormFieldBaseProps = {
   isOptional: boolean
   readonly?: boolean
   logger?: Logger
+  showLabel?: boolean
 }
 
 export function FormFieldBase(props: FormFieldBaseProps) {
   const { children, fieldName, fieldSchema, isOptional } = props
-  const metadata = getFieldMetadata(fieldSchema)
-  if (!metadata) throw new Error(`Field "${fieldName}" is missing metadata (label, placeholder, state)`)
-  const { label = '' } = metadata
-  const requiredMark = !isOptional && <span className="text-red-500 ml-1">*</span>
+  const metadata = getFieldMetadataOrThrow(fieldName, fieldSchema)
+  const indented = metadata.dependsOn !== undefined || metadata.isVisible !== undefined
   return (
     <FormField
       key={fieldName}
       name={fieldName}
       render={({ field }) => (
-        <FormItem>
-          <FormLabel>
-            {label}
-            {requiredMark}
-          </FormLabel>
+        <FormItem
+          className={cn('py-2 items-start', {
+            'indented border-l border-l-2 pl-5': indented,
+            'not-indented': !indented,
+          })}
+        >
+          {props.showLabel !== false && <FormFieldLabel isOptional={isOptional} label={metadata.label} />}
           {children?.(field)}
           <FormMessage name={fieldName} />
         </FormItem>
