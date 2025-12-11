c84fee68e13457f458db6f7f9406ac2c75c808da
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-12-11 17:27:44 +0100

    feat: add FormFieldLabel and RequiredMark components for form field labeling

---
+++ libs/components/src/molecules/form-field.utils.tsx
@@ -0,0 +1,14 @@
+import { FormLabel } from '../atoms/form'
+
+export function RequiredMark() {
+  return <span className="text-red-500 ml-1">*</span>
+}
+
+export function FormFieldLabel({ className, label, isOptional }: { className?: string; label?: string; isOptional: boolean }) {
+  return (
+    <FormLabel className={className}>
+      {label}
+      {!isOptional && <RequiredMark />}
+    </FormLabel>
+  )
+}
