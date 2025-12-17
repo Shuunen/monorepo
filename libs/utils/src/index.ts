51c1a5314bc574d2d3882ffa0436afd18c449234
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-12-17 14:52:48 +0100

    feat(react): add getNodeText function and corresponding tests

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -42,6 +42,7 @@ export * from "./lib/object-serializer.js";
 export * from "./lib/object-sort.js";
 export * from "./lib/objects.js";
 export * from "./lib/random.js";
+export * from "./lib/react.js";
 export * from "./lib/result.js";
 export * from "./lib/sleep.js";
 export * from "./lib/state.js";
---
+++ libs/utils/src/lib/react.test.ts
@@ -0,0 +1,64 @@
+import type { ReactNode } from "react";
+import { functionReturningVoid } from "./functions.js";
+import { getNodeText } from "./react.js";
+
+describe("react", () => {
+  it("getNodeText A string", () => {
+    expect(getNodeText("Hello World")).toBe("Hello World");
+  });
+
+  it("getNodeText B number", () => {
+    expect(getNodeText(42)).toBe("42");
+  });
+
+  it("getNodeText C boolean", () => {
+    expect(getNodeText(true)).toBe("");
+  });
+
+  it("getNodeText D undefined", () => {
+    expect(getNodeText(undefined)).toBe("");
+  });
+
+  it("getNodeText E array", () => {
+    expect(getNodeText(["Hello", " ", "World", "!"])).toBe("Hello World!");
+  });
+
+  it("getNodeText F React element with string child", () => {
+    const element = {
+      props: {
+        children: "Hello from React",
+      },
+    } as ReactNode;
+    expect(getNodeText(element)).toBe("Hello from React");
+  });
+
+  it("getNodeText G React element with nested children", () => {
+    const element = {
+      props: {
+        children: [
+          "Hello ",
+          {
+            props: {
+              children: "from ",
+            },
+          },
+          {
+            props: {
+              children: "React",
+            },
+          },
+        ],
+      },
+    } as ReactNode;
+    expect(getNodeText(element)).toBe("Hello from React");
+  });
+
+  it("getNodeText H un-handled object", () => {
+    const obj = { foo: "bar" } as unknown as ReactNode;
+    expect(getNodeText(obj)).toBe("un-handled object");
+  });
+
+  it("getNodeText I un-handled type (function)", () => {
+    expect(getNodeText(functionReturningVoid as unknown as ReactNode)).toBe("un-handled type");
+  });
+});
