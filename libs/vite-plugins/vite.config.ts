e313b2b45ab2cc1b2570e548d6ca42b52167b919
Romain Racamier-Lafon <romain.racamier@gmail.com>
2026-01-14 16:12:11 +0100

    chore: update coverage configuration to include defaults for exclusion

--- libs/vite-plugins/vite.config.ts
+++ libs/vite-plugins/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vitest/config";
+import { coverageConfigDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   cacheDir: "../../node_modules/.vite/libs/vite-plugins",
@@ -6,7 +6,7 @@ export default defineConfig({
   root: __dirname,
   test: {
     coverage: {
-      exclude: ["src/index.js", "*.config.ts", "src/**/*.d.ts"],
+      exclude: [...coverageConfigDefaults.exclude, "src/index.js", "*.config.ts", "src/**/*.d.ts"],
       include: ["src"],
       provider: "v8" as const,
       reporter: [["text", { maxCols: 120 }], "lcov"],
