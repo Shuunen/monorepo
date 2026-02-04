df0c98fbf5e0773c5fdd4f376016ba6ee1704e8d
Romain Racamier-Lafon <romain.racamier@gmail.com>
2026-02-04 10:43:42 +0100

    fix(scroll): update scrollToElement to use dynamic querySelector

--- libs/utils/src/lib/browser-scroll.ts
+++ libs/utils/src/lib/browser-scroll.ts
@@ -2,7 +2,7 @@
 import { invariant } from "es-toolkit";
 
 export function scrollToElement(querySelector: string) {
-  const element = globalThis.document.querySelector("[data-testid='IDTravelDocuments-0']");
+  const element = globalThis.document.querySelector(querySelector);
   invariant(element, `Cannot find element for query ${querySelector}`);
   element.scrollIntoView({
     behavior: "smooth",
