ca91e74e6adfd80e6939a371d0682afb7a0d6dfa
Romain Racamier-Lafon <romain.racamier@gmail.com>
2026-02-13 14:50:41 +0100

    fix(test): simplify componentRegistry import in auto-form-field tests

--- libs/components/src/molecules/auto-form-field.test.ts
+++ libs/components/src/molecules/auto-form-field.test.ts
@@ -1,8 +1,8 @@
 import { describe, expect, it } from "vitest";
+import { componentRegistry } from "./auto-form-field.utils";
 
 describe("auto-form-field.utils", () => {
-  it("should have componentRegistry defined", async () => {
-    const { componentRegistry } = await import("./auto-form-field.utils");
+  it("should have componentRegistry defined", () => {
     expect(componentRegistry).toBeDefined();
     expect(Object.keys(componentRegistry).length).toMatchInlineSnapshot(`11`);
   });
