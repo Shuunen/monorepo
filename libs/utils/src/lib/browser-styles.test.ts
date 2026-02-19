37d844356e601158dc35e9fb8b674ca3c9921b30
Romain Racamier-Lafon <romain.racamier@gmail.com>
2026-02-19 17:24:59 +0100

    chore: add cn tests

--- libs/utils/src/lib/browser-styles.test.ts
+++ libs/utils/src/lib/browser-styles.test.ts
@@ -1,9 +1,25 @@
 import { cn } from "./browser-styles.js";
 
-it("cn A", () => {
-  expect(cn("a", "b", "c")).toMatchInlineSnapshot(`"a b c"`);
-});
+describe("browser-styles", () => {
+  it("cn A", () => {
+    expect(cn("a", "b", "c")).toMatchInlineSnapshot(`"a b c"`);
+  });
 
-it("cn B", () => {
-  expect(cn("a", null, "b", "c", undefined)).toMatchInlineSnapshot(`"a b c"`);
+  it("cn B", () => {
+    expect(cn("a", null, "b", "c", undefined)).toMatchInlineSnapshot(`"a b c"`);
+  });
+
+  it("cn C empty input", () => {
+    expect(cn([])).toBe("");
+  });
+
+  it("cn D simple classes", () => {
+    expect(cn("bg-blue text-black", "text-red font-bold")).toMatchInlineSnapshot(`"bg-blue text-red font-bold"`);
+  });
+
+  it("cn E merge classes", () => {
+    expect(cn("bg-blue bg-blue bg-blue text-black", "text-red font-bold")).toMatchInlineSnapshot(
+      `"bg-blue text-red font-bold"`,
+    );
+  });
 });
