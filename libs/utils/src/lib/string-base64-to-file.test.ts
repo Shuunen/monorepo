2efdf1c77d8f0344fb44877f0dfef3abe9ff1b6a
Romain Racamier-Lafon <romain.racamier@gmail.com>
2026-03-11 14:58:31 +0100

    fix: update base64ToFile function to include filename extension check and parameter

--- libs/utils/src/lib/string-base64-to-file.test.ts
+++ libs/utils/src/lib/string-base64-to-file.test.ts
@@ -2,16 +2,29 @@ import { Result } from "./result.js";
 import { base64ToFile } from "./string-base64-to-file.js";
 
 describe(base64ToFile, () => {
-  it("should convert successfully", () => {
+  it("A should convert successfully", () => {
     const base64GifString = "data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==";
-    const result = Result.unwrap(base64ToFile(base64GifString, "file"));
+    const result = Result.unwrap(base64ToFile(base64GifString, "file.gif"));
     expect(result.value instanceof File).toBe(true);
     expect(result.value?.name).toBe("file.gif");
   });
 
-  it("should not encode", () => {
+  it("B bad format, should not encode", () => {
     const base64GifString = "R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==";
-    const result = Result.unwrap(base64ToFile(base64GifString, "file"));
+    const result = Result.unwrap(base64ToFile(base64GifString, "file.gif"));
     expect(result.error).toMatchInlineSnapshot(`"Failed to encode base64 string."`);
+  });
+
+  it("C missing extension in filename", () => {
+    const base64GifString = "data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==";
+    const result = Result.unwrap(base64ToFile(base64GifString, "file"));
+    expect(result.error).toMatchInlineSnapshot(`"Missing extension in the filename."`);
+  });
+
+  it("D missing extension in filename on purpose", () => {
+    const base64GifString = "data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==";
+    const result = Result.unwrap(base64ToFile(base64GifString, "foobar", false));
+    expect(result.value instanceof File).toBe(true);
+    expect(result.value?.name).toBe("foobar");
   });
 });
