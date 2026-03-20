460cf705240e6987098adf39c3941fad0ff4e8b3
Romain Racamier-Lafon <romain.racamier@gmail.com>
2026-03-20 10:02:47 +0000

    wip 1

--- libs/utils/src/lib/object-is-record.ts
+++ libs/utils/src/lib/object-is-record.ts
@@ -1,8 +1,1 @@
-/**
- * Check if a value is an object/record
- * @param value the value to check
- * @returns true if value is an object/record
- */
-export function isRecord(value: unknown): value is Record<string, unknown> {
-  return value !== null && typeof value === "object" && !Array.isArray(value);
-}
+export { isPlainObject as isRecord } from "es-toolkit";
