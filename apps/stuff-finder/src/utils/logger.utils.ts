f9417e4a79ffe7d64aace9bff951f6dc807406c0
Romain Racamier-Lafon <romain.racamier@gmail.com>
2026-04-03 10:53:32 +0200

    fix: enhance error logging for unhandled promise rejections and errors

--- apps/stuff-finder/src/utils/logger.utils.ts
+++ apps/stuff-finder/src/utils/logger.utils.ts
@@ -1,3 +1,14 @@
 import { Logger } from "@monorepo/utils";
 
 export const logger = new Logger();
+
+/* v8 ignore start */
+if (globalThis.window) {
+  globalThis.window.addEventListener("unhandledrejection", event => {
+    logger.error("unhandled rejection", event.reason);
+  });
+
+  globalThis.window.addEventListener("error", event => {
+    logger.error("unhandled error", event.error);
+  });
+}
